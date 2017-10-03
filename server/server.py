2f3b35f3a1aa32a1db47b78ba2ba1eac7ffeda38
Mathieu Leplatre <mathieu@mozilla.com>
2017-10-03 17:39:51 +0200

    Rewrite decorator

--- server/server.py
+++ server/server.py
@@ -1,12 +1,15 @@
+#
+# https://auth0.com/docs/quickstart/backend/python#add-api-authorization
+# https://github.com/auth0-samples/auth0-python-api-samples/tree/master/00-Starter-Seed
+
 import functools
 import json
 import re
 import os
-from urllib.request import urlopen
+from urllib.request import urlopen, Request
 
 from flask import Flask, request, jsonify, _app_ctx_stack
 from flask_cors import cross_origin
-from jose import jwt
 
 
 AUTH0_DOMAIN = os.getenv("AUTH0_DOMAIN")
@@ -29,95 +32,40 @@ def handle_auth_error(ex):
     return response
 
 
-def get_token_auth_header():
-    """Obtains the access token from the Authorization Header
-    """
-    auth = request.headers.get("Authorization", None)
-    if auth is None:
-        raise AuthError({
-            "code": "authorization_header_missing",
-            "description": "Authorization header is expected"
-        }, 401)
+def authorized(resource, action):
+    def wrapped(function):
+        @functools.wraps(function)
+        def wrapper(*args, **kwargs):
+            body = json.dumps({
+                "resource": resource,
+                "action": action,
+            })
+            headers = {
+                "Authorization": request.headers.get("Authorization", None),
+                "Auth0-Audience": API_AUDIENCE,
+                "Auth0-Domain": AUTH0_DOMAIN,
+            }
+            r = Request(IAM_SERVER + "/allowed", data=body, headers=headers)
+            resp = urlopen(r)
+            payload = json.loads(resp.read().decode("utf-8"))
 
-    s = re.match(r"^Bearer ([^\s]+)$", auth, re.I)
-    if s is None:
-        raise AuthError({
-            "code": "invalid_header",
-            "description": "Authorization header must be 'Bearer <token>'"
-        }, 401)
+            if not payload["allowed"]:
+                raise AuthError({
+                    "code": "not_allowed",
+                    "description": "This JWT Access Token is not authorized."
+                }, 403)
 
-    token = s.group(1)
-    return token
+            _app_ctx_stack.top.current_user = payload
+            return f(*args, **kwargs)
 
-
-def requires_auth(f):
-    """Determines if the access token is valid
-    """
-    @functools.wraps(f)
-    def decorated(*args, **kwargs):
-        token = get_token_auth_header()
-        try:
-            unverified_header = jwt.get_unverified_header(token)
-        except jwt.JWTError as e:
-            raise AuthError({
-                "code": "invalid_header",
-                "description": "Invalid header. " + str(e)
-            }, 401)
-        if unverified_header["alg"] != "RS256":
-            raise AuthError({
-                "code": "invalid_header",
-                "description": "Invalid header. Use an RS256 signed JWT Access Token"
-            }, 401)
-
-        jsonurl = urlopen("https://" + AUTH0_DOMAIN + "/.well-known/jwks.json")
-        jwks = json.loads(jsonurl.read().decode("utf-8"))
-        rsa_key = None
-        for key in jwks["keys"]:
-            if key["kid"] == unverified_header["kid"]:
-                rsa_key = {
-                    "kty": key["kty"],
-                    "kid": key["kid"],
-                    "use": key["use"],
-                    "n": key["n"],
-                    "e": key["e"]
-                }
-        if rsa_key is None:
-            raise AuthError({
-                "code": "invalid_header",
-                "description": "Unable to find appropriate key"
-            }, 400)
-        try:
-            payload = jwt.decode(token,
-                                 rsa_key,
-                                 algorithms=ALGORITHMS,
-                                 audience=API_AUDIENCE,
-                                 issuer="https://" + AUTH0_DOMAIN + "/")
-        except jwt.ExpiredSignatureError:
-            raise AuthError({
-                "code": "token_expired",
-                "description": "Token is expired"
-            }, 401)
-        except jwt.JWTClaimsError:
-            raise AuthError({
-                "code": "invalid_claims",
-                "description": "Incorrect claims, please check the audience and issuer"
-            }, 401)
-        except Exception:
-            raise AuthError({
-                "code": "invalid_header",
-                "description": "Unable to parse authentication token."
-            }, 400)
-
-        _app_ctx_stack.top.current_user = payload
-        return f(*args, **kwargs)
-
-    return decorated
+        return wrapper
+    return wrapped
 
 
 @APP.route("/")
 @cross_origin(headers=["Content-Type", "Authorization"])
 @cross_origin(headers=["Access-Control-Allow-Origin", "*"])
-@requires_auth
+@authorized(resource="demo:hello", action="read")
 def hello():
     """A valid access token is required to access this route
     """
